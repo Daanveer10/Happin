2a4e557fa95861abfe9e9fe72fd3fd15921d1294
Daanveer10 <daanveer28@gmail.com>
2025-12-09 19:03:04 +0530

    Update .layout.tsx

--- .src/.app/.layout.tsx
+++ .src/.app/.layout.tsx
@@ -1,1 +1,13 @@
+export const metadata = {
+  title: "Happin",
+  description: "Discover real experiences happening around you",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
 
