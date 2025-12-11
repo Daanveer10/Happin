d151b2a37455c06d0f0ac949ea2a241152cae9e3
Daanveer10 <daanveer28@gmail.com>
2025-12-11 18:11:27 +0530

    Create index.js

---
+++ .src/.pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.tsx
+import React from "react";
+
+export default function Home() {
+  return (
+    <main style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh", fontFamily: "Inter, system-ui, sans-serif" }}>
+      <h1>Welcome — pages/index (TypeScript)</h1>
+    </main>
+  );
+}
