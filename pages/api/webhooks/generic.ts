7897c76c34b44e087e2380086bb409b539a60a74
Daanveer10 <daanveer28@gmail.com>
2025-12-12 18:57:10 +0530

    Update generic.ts

--- pages/api/webhooks/generic.ts
+++ pages/api/webhooks/generic.ts
@@ -1,6 +1,6 @@
 
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getFirestore } from "@/lib/firebase";
+import { getFirestore } from "../../../lib/firebase";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
